6ca4467ef77794201d254838b4b0eb1815665635
Rafał <rafal.klocek.praca@gmail.com>
2023-06-15 23:59:31 +0200

    added nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,9 @@
 const header = document.querySelector(".header");
-const footerYear = document.querySelector(".footer__year")
+const footerYear = document.querySelector(".footer__year");
+const navSide = document.querySelector(".nav-side");
+const navSideBg = document.querySelector(".nav-side__bg");
+const navSideBtn = document.querySelector(".nav__hamburger");
+const navSideCloseBtn = document.querySelector(".nav-side__closebutton");
 
 const changePictureOfHeader = () => {
 	if (window.screen.width < 992) {
@@ -10,10 +14,20 @@ const changePictureOfHeader = () => {
 };
 
 const setFooterYear = () => {
-	const date = new Date()
+	const date = new Date();
 
 	footerYear.textContent = date.getFullYear();
-}
+};
+
+const toggleNavSide = () => {
+	navSide.classList.toggle("hidden");
+	navSideBg.classList.toggle("hidden");
+	document.body.classList.toggle("body-scroll");
+};
 
 changePictureOfHeader();
-setFooterYear()
+setFooterYear();
+
+navSideBtn.addEventListener("click", toggleNavSide);
+navSideCloseBtn.addEventListener("click", toggleNavSide);
+navSideBg.addEventListener("click", toggleNavSide);
