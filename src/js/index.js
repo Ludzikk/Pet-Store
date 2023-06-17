ab68cb100773889529196f988b9912af4bf12982
Rafał <rafal.klocek.praca@gmail.com>
2023-06-17 18:32:08 +0200

    added categories subpages for every kind of pets

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ const navSide = document.querySelector(".nav-side");
 const navSideBg = document.querySelector(".nav-side__bg");
 const navSideBtn = document.querySelector(".nav__hamburger");
 const navSideCloseBtn = document.querySelector(".nav-side__closebutton");
+const productsCount = document.querySelector(".main__productscount");
 
 const changePictureOfHeader = () => {
 	if (window.screen.width < 992) {
@@ -25,8 +26,18 @@ const toggleNavSide = () => {
 	document.body.classList.toggle("body-scroll");
 };
 
+const checkProductsCount = () => {
+	productsCount.textContent = `${productsCount.parentElement.nextElementSibling.childElementCount} products`;
+};
+
 changePictureOfHeader();
 setFooterYear();
+
+if(document.title.includes("Dog") || document.title.includes("Cat") || document.title.includes("Aquarium") || document.title.includes("Reptile") || document.title.includes("Small") || document.title.includes("Bird")) {
+	checkProductsCount();
+} else {
+	console.log("Not Running Function");
+}
 
 navSideBtn.addEventListener("click", toggleNavSide);
 navSideCloseBtn.addEventListener("click", toggleNavSide);
